9c6f88274a7aa3c3621758eca89b0d68e56fe61f
cafakleinn <cafakleinn@gmail.com>
2025-08-19 22:13:43 -0400

    Add departure time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 verify_departing_city = 'N'
+verify_departing_time = 'N'
 
 print("Welcome to the Flight Time Calculator. ")
 
+# Departing City
 while verify_departing_city != 'Y' or verify_departing_city != 'y':
     departing_city = input("Please enter the departing city you are flying from: ")
     verify_departing_city = input(f"The departing city you inputted is {departing_city}. Is this correct? (Y/N) ")
@@ -9,6 +11,11 @@ while verify_departing_city != 'Y' or verify_departing_city != 'y':
     if verify_departing_city == 'Y' or verify_departing_city == 'y':
         break
 
-# print("Welcome to the Flight Time Calculator. Please enter the departure time:")
-# departure_time = input()
-# print(f"The departure time you ")
+# Departure Time
+while (verify_departing_time != 'Y' or verify_departing_time != 'y'):
+    departure_time = input("Please enter the departure time: ")
+    verify_departing_time = input(f"The departure time you inputted is {departure_time}. Is this correct? (Y/N) ")
+
+    if verify_departing_time == 'Y' or verify_departing_time == 'y':
+        break
+
